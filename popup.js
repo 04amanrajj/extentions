681680787c1b9b64e4d92762920763fbfa46a482
04amanrajj <04.aman.raj@gmail.com>
2025-03-04 17:37:09 +0530

    script update

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("save").addEventListener("click", () => {
-    const currency = document.getElementById("currency").value;
-    chrome.storage.sync.set({ selectedCurrency: currency }, () => {
-        alert("Currency saved!");
-    });
-});
